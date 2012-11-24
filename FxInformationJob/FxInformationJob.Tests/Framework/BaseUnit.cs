dd48d6b3ad39e4b4ada248cb9d695e5c6065a5f6
chenzhuojie <117822597@163.com>
2012-11-24 14:34:23 +0800

    引用了最新的服务组件
    完善了一些配置信息和正确的调度任务测试代码

---
+++ FxInformationJob/FxInformationJob.Tests/Framework/BaseUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FxInformationJob.App_Start;
+
+namespace FxInformationJob.Tests.Framework
+{
+    public class BaseUnit
+    {
+        public BaseUnit()
+        {
+            SimpleInjectorInitializer.Initialize();
+        }
+    }
+}
---
+++ FxInformationJob/FxInformationJob.Tests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FxInformationJob.Tests.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FxInformationJob.Tests
+{
+    [TestClass]
+    public class UnitTest1 : BaseUnit
+    {
+        [TestMethod]
+        public void RunAll()
+        {
+            RunGoodsTransfer();
+            RunGoodsBuy();
+            RunCarBuy();
+            RunCarTransfer();
+            RunHouseBuy();
+            RunHouseTransfer();
+        }
+
+
+        [TestMethod]
+        public void RunGoodsTransfer()
+        {
+            new FxTask.FxGoods.Transfer.GoodsTransferJobLoad().Execute();
+        }
+
+        [TestMethod]
+        public void RunGoodsBuy()
+        {
+            new FxTask.FxGoods.Buy.GoodsBuyJobLoad().Execute();
+        }
+
+        [TestMethod]
+        public void RunCarBuy()
+        {
+            new FxTask.FxCar.Buy.CarBuyJobLoad().Execute();
+        }
+
+        [TestMethod]
+        public void RunCarTransfer()
+        {
+            new FxTask.FxCar.Transfer.CarTransferJobLoad().Execute();
+        }
+
+        [TestMethod]
+        public void RunHouseBuy()
+        {
+            new FxTask.FxHouse.Buy.HouseBuyJobLoad().Execute();
+        }
+
+        [TestMethod]
+        public void RunHouseTransfer()
+        {
+            new FxTask.FxHouse.Transfer.HouseTransferJobLoad().Execute();
+        }
+    }
+}
